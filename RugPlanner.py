1c92c8aa35edd8a8096ac1823777086907c430a9
Laura Ekstrand <laura@jlekstrand.net>
2022-07-17 16:30:17 -0500

    Draws horizontal bands of specified webcolor or hexcolor.

--- RugPlanner.py
+++ RugPlanner.py
@@ -2,8 +2,10 @@ import cv2
 import random
 import csv
 import numpy as np
+import webcolors as wbc
 
 ragcolors = []
+raghex = {}
 ragcounts = []
 
 # Read the Input.csv
@@ -12,7 +14,8 @@ with open('Input.csv', newline='') as csvfile:
     for row in csvreader:
         print(row)
         ragcolors.append(row[0])
-        ragcounts.append(int(row[1]))
+        raghex[row[0]] = row[1]
+        ragcounts.append(int(row[2]))
 
 print(ragcolors)
 print(ragcounts)
@@ -20,9 +23,22 @@ ragtotal = sum(ragcounts)
 ragrows = random.sample(ragcolors, k=ragtotal, counts=ragcounts)
 print(ragrows)
 
-#img = cv2.imread('Output.png')
-img = np.zeros((512,512,3), np.uint8)
-cv2.line(img, (0,0), (511, 0), (255,0,0), 5)
+width = 1024
+height = 2048
+img = np.zeros((height,width,3), np.uint8)
+x = 0
+stripeHeight = int(height/ragtotal)
+print(stripeHeight)
+for row in ragrows:
+    rowcolor = wbc.name_to_rgb('Black')
+    if (raghex[row] == ''):
+        rowcolor = wbc.name_to_rgb(row)
+    else:
+        rowcolor = wbc.hex_to_rgb(raghex[row])
+    cv2.line(img, (0,x), (height-1, x), (rowcolor.blue, rowcolor.green, rowcolor.red), stripeHeight)
+    x = x + stripeHeight
+
+cv2.imwrite('Output.png', img)
 cv2.imshow('Output', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
